58077155c4828b4ea561473ef115a88e76064472
lucaruboni <lucaruboni@gmail.com>
2023-03-23 12:42:46 +0100

    final commit snack 2

---
+++ snack2/main.js
@@ -0,0 +1,36 @@
+/* A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+
+Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’] */
+
+
+
+
+const stringList = [
+    'pippo',
+    'PLuto',
+    'PAPERINO'
+]
+
+
+
+
+stringList.forEach((element, index) => {
+    
+
+    newElement0 = stringList[0].replace('pippo', 'Pippo')
+    
+
+    newElement1 = stringList[1].replace('PLuto', 'Pluto')
+    
+
+    newElement2 = stringList[2].replace('PAPERINO', 'Paperino')
+    
+    element = [newElement0, newElement1, newElement2]
+
+    console.log(element[index])
+});
+   
+
+
+
+
